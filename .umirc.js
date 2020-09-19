dc0b5d9cb25849fe9b4b06b36584906cbd88fa9d
xiaolong666666 <long.1585097369@qq.com>
2020-09-19 17:11:20 +0800

    初始化恬逸小岛

---
+++ .umirc.js
@@ -0,0 +1,38 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' }
+      ]
+    }
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      links: [{ rel: 'icon', href: 'assets/favicon.ico' }],
+      title: '恬逸小岛-体验风土，感受人情，恬静舒适',
+      dll: true,
+      locale: {
+        enable: true,
+        default: 'en-US',
+      },
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
